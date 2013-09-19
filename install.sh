309258ea1cddbdb1fe0488b94f38bbc14df088f2
Tom Robertson <tr51@hw.ac.uk>
2013-09-19 12:18:17 +0100

    moved dotfiles to /dotfiles

--- install.sh
+++ install.sh
@@ -3,13 +3,22 @@
 
 cd ~
 
-#git clone https://
+git clone https://github.com/tar672/dotfiles.git .dotfiles
 
 cd .dotfiles
 
 for file in *
 do
-  mv "$file" "../.$file"
+	if [ -f "$file" ]; then
+		echo "Overwrote $file"
+		mv "../$file" ./.old
+  fi
+done
+echo "old files are saved in ~/.dotfiles/.old
+
+for file in *
+do
+  cp "$file" "../.$file"
 done
 
 source ~/.bashrc
