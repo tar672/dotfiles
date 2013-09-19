d568d038cdd4510083b9f202b922029addee5501
Tom Robertson <tr51@hw.ac.uk>
2013-09-19 13:13:34 +0100

    testing

--- install.sh
+++ install.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-git clone https://github.com/tar672/dotfiles.git ~/.dotfiles
+if [ -d ~/.dotfiles ]; then
+  cd ~/.dotfiles
+  git pull origin
+else
+  git clone https://github.com/tar672/dotfiles.git ~/.dotfiles
+fi
 
 cd ~/.dotfiles/dotfiles
 for file in *
 do
-	if [ -f "~/.$file" ]; then
+	if [ -f ~/.$file ]; then
 		echo "Overwrote $file"
-		mv "~/.$file" ../.old
+		mv ~/.$file ../.old
   fi
 done
 echo "old files are saved in ~/.dotfiles/.old"
 
 for file in *
 do
-  cp "$file" "~/.$file"
+	cat $file
+  if [ -d $file ]; then
+		ln -s -f $file ~/.$file
+  else
+    cp -f $file ~/.$file
+  fi
 done
 
 source ~/.bashrc
