f037138b1624d5b98a5c816a763f5378c156cbb1
Tom Robertson <tr51@hw.ac.uk>
2013-09-19 15:17:36 +0100

    now clones with submodules

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ -d ~/.dotfiles ]; then
   cd ~/.dotfiles
   git pull origin
 else
-  git clone https://github.com/tar672/dotfiles.git ~/.dotfiles
+  git clone --recursive https://github.com/tar672/dotfiles.git ~/.dotfiles
 fi
 
 cd ~/.dotfiles/dotfiles
@@ -19,12 +19,7 @@ echo "old files are saved in ~/.dotfiles/.old"
 
 for file in *
 do
-	cat $file
-  if [ -d $file ]; then
-		ln -s -f $file ~/.$file
-  else
-    cp -f $file ~/.$file
-  fi
+	ln -s -f ~/.dotfiles/dotfiles/$file ~/.$file
 done
 
 source ~/.bashrc
